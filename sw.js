b85af6cd1e1d5d998317b9db5a964c2c63eb4122
dorldorglobal-cpu <dorldorglobal@gmail.com>
2026-08-17 17:00:45 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='crapless-craps-v4';
+const CACHE='crapless-craps-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
